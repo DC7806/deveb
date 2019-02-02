b0911ff09fb874d7b0940112cd81233a0a7ff5f7
DC <dachang.h@gmail.com>
2019-02-03 01:55:34 +0800

    Add render_page_title helper

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,9 +1,16 @@
 module ApplicationHelpers
-def render_repo_url(repo)
-  return if repo.nil?
+  def render_repo_url(repo)
+    return if repo.nil?
 
-  link_to "#{ENV['GITHUB_ACCOUNT']}/#{repo}", target: :_blank, class: 'button showcase__button is-medium' do
-    content_tag :i, '', class: 'fab fa-github'
+    link_to "#{ENV['GITHUB_ACCOUNT']}/#{repo}", target: :_blank, class: 'button showcase__button is-medium' do
+      content_tag :i, '', class: 'fab fa-github'
+    end
   end
-end
+
+  def render_page_title(page_title=nil)
+    site_name = ENV['SITE_NAME']
+    
+    return site_name if page_title.nil?
+    "#{page_title} | #{site_name}"
+  end
 end
